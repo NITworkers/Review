f61e2c2ce9ba3732f26ae8e74ddc16ace54eb15c
NITworkers <ymk-t@hotmail.com>
2020-03-16 03:56:37 +0000

    move old files

---
+++ _old/Pages/js/top.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Top extends React.Component{
+  render(){
+    return(
+      <div>レビューサイト</div>
+
+    );
+  }
+}
+
+ReactDOM.render(<Top />, document.getElementById("content"))
